18be07446ce9db0c1a8d6cb21e188774506af77e
vhnghi <vanhuunghiadhktpm13a@gmail.com>
2020-10-04 10:28:32 +0700

    add file index

---
+++ index.js
@@ -0,0 +1,17 @@
+var http = require('http');
+const port = 3001;
+var url = require('url');
+const server = http.createServer((request,response)=>{
+   //response.write('this is a response for a request!');
+   response.writeHead(200,{'Content-Type':'text/html'});
+   //const ipAddress = request.socket.remoteAddress;
+//    response.write(`Your IP address is ${ipAddress}`);
+//    response.write(`response url ${request.url}`);
+//    response.write(`Detail request's url: ${require('url').parse(request.url,true)}`);
+    var q = url.parse(request.url,true).query;
+    var txt = q.name + " " + q.email;
+    response.write(txt);
+    response.end();
+}).listen(port);
+
+console.log(`server is running port:${port}.`);
